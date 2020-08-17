d6bc5f7fe8e35fa2cb78cd031c425c7dddef85b3
karthik-karotra <karthikpatel54@gmail.com>
2020-08-17 19:22:19 +0530

    [Karthik]ADDED : Cab Invoice Generator Project Files

---
+++ CabInvoiceGenerator/CabInvoiceGenerator.cs
@@ -0,0 +1,12 @@
+﻿// <copyright file="CabInvoiceGenerator.cs" company="BridgeLabz Solution">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+namespace CabInvoiceGenerator
+{
+    /// <summary>
+    /// Cab Invoicer Generator Service Class.
+    /// </summary>
+    public class CabInvoiceGenerator
+    {
+    }
+}
---
+++ CabInvoiceGeneratorTest/CabInvoiceGeneratorTest.cs
@@ -0,0 +1,31 @@
+﻿// <copyright file="CabInvoiceGeneratorTest.cs" company="BridgeLabz Solution">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
+namespace CabInvoiceGeneratorTest
+{
+    using NUnit.Framework;
+
+    /// <summary>
+    /// Cab Invoice Generator Test Class.
+    /// </summary>
+    public class CabInvoiceGeneratorTest
+    {
+        /// <summary>
+        /// SetUp Method.
+        /// </summary>
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        /// <summary>
+        /// Test Method.
+        /// </summary>
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
